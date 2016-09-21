87aa33eb6a9ca6564b075fae3128163c72d31dd8
jeremiahfernandezzzz <jeremiahfernandezzzz@gmail.com>
2016-09-21 10:32:49 +0800

    Update main.js

--- bio/script/main.js
+++ bio/script/main.js
@@ -260,6 +260,30 @@ function runTheseFunctionsOnKeyDown(e){
 	objectDown("#otherskill-set", -5650);
 	objectUp("#contact-form", -6750);
 }
+
+function runTheseFunctionsOnMouseDown(){
+	xCtr();
+	hideInitialDialog(),
+	switchChar(-1760,-3020,-4270),
+	moveBgMouseDown(), 
+	moveObjectsMouseDown(), 
+	kramerAnimateMouseDown(), 
+	changeDialog("I'm 19<br/>years old", -1490, 450), 
+	changeDialog("Went to school<br/>at these years", -2470, 600), 
+	changeDialog("Then I went<br/>to college", -3650, 600), 
+	changeDialog("I shifted<br/>courses", -4300, 300), 
+	changeDialog("mech. eng.<br/>is hard", -4600, 200), 
+	changeDialog("<br/>i haz skillz", -5050, 450), 
+	changeDialog("moar skillz", -5700, 450) ,
+	changeDialog("Though there's<br/>a lot to learn", -6550, 225) ,
+	changeDialog("press tab<br/>to navigate", -7100, 225) ,
+	objectUp("#hospital", -1250), 
+	objectUp("#grade-school", -2450), 
+	objectUp("#high-school", -3650);
+	objectDown("#skill-set", -4800);
+	objectDown("#otherskill-set", -5650);
+	objectUp("#contact-form", -6750);
+}
   
 function runTheseFunctionsOnKeyUp(e){
 	kramerStatic();
