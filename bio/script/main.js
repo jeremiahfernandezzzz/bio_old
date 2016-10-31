2f42e27705db4a1e57b76152c149ea4b7779fc56
jeremiahfernandezzzz <jeremiahfernandezzzz@gmail.com>
2016-11-01 01:58:11 +0800

    Update main.js

--- bio/script/main.js
+++ bio/script/main.js
@@ -67,50 +67,6 @@ function forward(e) {
 	}
 }
 
-/*function touchForward(){
-	//from moveBg - moves primary background
-	if (xCtrBg > -7100) {
-		forwardObject(".object-parallax", 10);
-		forwardObject(".object-parallax2", 10);
-		document.querySelector(".bg").style.transform = 'translateX(' + xCtrBg + 'px)';
-		xCtrBg -= 10;
-	}
-	
-	function forwardObject(object, pixels){
-		document.querySelector(object).style.transform = 'translateX(' + xCtrObjects + 'px)';
-		xCtrObjects -= pixels;
-	}
-	
-	if (xCtrBg <= -1340) {
-		document.querySelector("#sun").style.transform = 'translate(' + xCtrSun + 'px, ' + yCtrSun + 'px)';
-		xCtrSun += 12;
-		if (xCtrSun < 3500){
-			yCtrSun -= 6;
-		} else {
-			yCtrSun += 6;
-		}
-	}
-	
-	if (xCtrBg < -6260) {
-		document.querySelector("#moon").style.transform = 'translate(' + xCtrMoon + 'px, ' + yCtrMoon + 'px)';
-		yCtrMoon -= 6;
-		xCtrMoon -= 10;
-	}
-	
-	if (charSwitchVal == 0) {	
-		document.querySelector("#kramer").style.animation =  "animate-char 1.0s steps(4) infinite"; //kramer animation
-	}
-	if (charSwitchVal == -750) {
-		document.querySelector("#kramer").style.animation =  "animate-char3 1.0s steps(4) infinite"; //kid animation
-	}
-	if (charSwitchVal == -1500) {
-		document.querySelector("#kramer").style.animation =  "animate-char5 1.0s steps(4) infinite"; //kid animation
-	}
-	if (charSwitchVal == -2250) {
-		document.querySelector("#kramer").style.animation =  "none"; //kid animation
-	}
-}*/
-
 function backward(e){
 		
 	function backwardObject(object, pixels){
@@ -261,9 +217,6 @@ function runTheseFunctionsOnLoad(e){
 	kramerStatic();
 }
 
-window.onkeydown = runTheseFunctionsOnKeyDown();
-
-window.addEventListener("touchstart", touchstart, false);
 window.addEventListener("load", runTheseFunctionsOnLoad, false);
 window.addEventListener("keydown", runTheseFunctionsOnKeyDown, false);
 window.addEventListener("keyup", runTheseFunctionsOnKeyUp, false);
