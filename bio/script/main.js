112e19ddd1c20945e794ac6b4519604736df0091
jeremiahfernandezzzz <jeremiahfernandezzzz@gmail.com>
2016-11-10 13:44:28 +0800

    Update main.js

--- bio/script/main.js
+++ bio/script/main.js
@@ -164,7 +164,7 @@ function hideDialog() {
 }
 
 function initialDialog () {
-	document.querySelector(".dialog").innerHTML = "<br/>Sup!<br />Hold ->" ;
+	document.querySelector(".dialog").innerHTML = "<br/>Sup!<br />touch/click to ->" ;
 }
 
 function hideInitialDialog () {
