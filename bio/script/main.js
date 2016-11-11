ee4f958946663a0b24ac9f50048b798af490bbfe
jeremiahfernandezzzz <jeremiahfernandezzzz@gmail.com>
2016-11-11 11:10:30 +0800

    Update main.js

--- bio/script/main.js
+++ bio/script/main.js
@@ -216,7 +216,7 @@ function runTheseFunctionsOnKeyDown(e){
 */
   
 function runTheseFunctionsOnLoad() {
-	window.setInterval(auto, 25);
+	window.setInterval(auto, 50);
 	initialDialog();
 	showDialog();
 	kramerStatic();
